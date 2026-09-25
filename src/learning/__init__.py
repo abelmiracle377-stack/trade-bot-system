8ac090bb87d22025089ffaeccab4b133caf79c4e
abelmiracle377-stack <abelmiracle377@gmail.com>
2026-09-25 17:08:16 +0100

    Expose feedback learner

--- src/learning/__init__.py
+++ src/learning/__init__.py
@@ -1,5 +1,6 @@
 """Signal outcome recording and model-learning primitives."""
 
+from .engine import FeedbackLearner
 from .store import SignalOutcome, SignalOutcomeStore
 
-__all__ = ["SignalOutcome", "SignalOutcomeStore"]
+__all__ = ["FeedbackLearner", "SignalOutcome", "SignalOutcomeStore"]
